3be3da5e92a88e9b50dd95b7198ae145f2ec95e7
tusharparmar10050 <tusharparmar10050@gmail.com>
2023-01-19 14:26:37 +0530

    buffer changes

--- public/code.js
+++ public/code.js
@@ -43,7 +43,7 @@
 			shareFile({
 				filename: file.name,
 				total_buffer_size:buffer.length,
-				buffer_size:4096,
+				buffer_size:524288,
 			}, buffer, el.querySelector(".progress"));
 		}
 		reader.readAsArrayBuffer(file);
